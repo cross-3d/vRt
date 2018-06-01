be436c25d008ea676f8a7cd036f5ac0f305189cd
Elvira Herosimenko <33594593+elviras9t@users.noreply.github.com>
2018-06-01 18:46:19 +0800

    use enum typing

--- include/vRt/vRt.h
+++ include/vRt/vRt.h
@@ -9,7 +9,7 @@ namespace vt { // store in official namespace
     // use 0x11E for VtResult
     // use 0x11F for VtStructureType
 
-    typedef enum VtResult {
+     enum VtResult: uint32_t {
         VT_SUCCESS = 0x11E00000, // default status
         VT_NOT_READY = 0x11E00001, 
         VT_TIMEOUT = 0x11E00002, 
@@ -17,12 +17,12 @@ namespace vt { // store in official namespace
         VT_ERROR_INITIALIZATION_FAILED = 0x11E00004, // if error occurs from ray tracer itself
     };
 
-    typedef enum VtPipelineBindPoint {
+     enum VtPipelineBindPoint: uint32_t {
         VT_PIPELINE_BIND_POINT_RAY_TRACING = 0x11F00000,
         VT_PIPELINE_BIND_POINT_ACCELERATOR = 0x11F00001 // unknown
     };
 
-    typedef enum VtStructureType {
+     enum VtStructureType: uint32_t {
         VT_STRUCTURE_TYPE_INSTANCE_CREATE_INFO = 0x11F00000,
         VT_STRUCTURE_TYPE_DEVICE_CREATE_INFO = 0x11F00001,
         VT_STRUCTURE_TYPE_DEVICE_CONVERT_INFO = 0x11F00002, 
@@ -37,12 +37,12 @@ namespace vt { // store in official namespace
     };
 
     // it is bitfield-based value
-    typedef enum VtFormat {
+     enum VtFormat: uint32_t {
 
     };
 
     // all supported topologies
-    typedef enum VtTopologyType {
+     enum VtTopologyType: uint32_t {
         VT_TOPOLOGY_TYPE_TRIANGLES_LIST = 0x11F00000
     };
 
