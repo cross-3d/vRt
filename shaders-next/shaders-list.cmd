c2223757c61cc354bf191f8561c68f3833761dcd
Elv. H <elviras9t@gmail.com>
2018-06-17 22:56:58 +0800

    optimize interpolator

--- shaders-next/shaders-list.cmd
+++ shaders-next/shaders-list.cmd
@@ -93,6 +93,7 @@ set OPTFLAGS= ^
 --scalar-replacement ^
 --simplify-instructions
 
+call spirv-opt %FIXFLAGS% %OUTDIR%%HLBV%interpolator.comp.spv    -o %OUTDIR%%HLBV%interpolator.comp.spv
 call spirv-opt %FIXFLAGS% %OUTDIR%%HLBV%traverse-bvh.comp.spv    -o %OUTDIR%%HLBV%traverse-bvh.comp.spv
 call spirv-opt %FIXFLAGS% %OUTDIR%%HLBV%bvh-build.comp.spv       -o %OUTDIR%%HLBV%bvh-build.comp.spv
 call spirv-opt %FIXFLAGS% %OUTDIR%%HLBV%bound-calc.comp.spv      -o %OUTDIR%%HLBV%bound-calc.comp.spv
