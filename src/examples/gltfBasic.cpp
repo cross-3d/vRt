bb8b2591d698594366444076afcc0ec9892f1905
Elv. H <elviras9t@gmail.com>
2018-07-07 21:02:26 +0800

    test on sponza again

--- src/examples/gltfBasic.cpp
+++ src/examples/gltfBasic.cpp
@@ -211,8 +211,9 @@ void main() {
     tinygltf::Model model = {};
     tinygltf::TinyGLTF loader = {};
     std::string err;
+    std::string input_filename("models/sponza/sponza.gltf");
     //std::string input_filename("models/BoomBoxWithAxes.gltf");
-    std::string input_filename("models/Chess_Set.gltf");
+    //std::string input_filename("models/Chess_Set.gltf");
     //std::string input_filename("models/Cube.gltf");
     //std::string input_filename("models/scene.gltf");
     //std::string input_filename("models/BoomBox.gltf");
@@ -338,10 +339,10 @@ void main() {
     }
 
 
-    //glm::vec3 eyePos = glm::vec3(0.f, 10.5f, -40.6f);
-    glm::vec3 eyePos = glm::vec3(0.f, 1.0f, 10.6f);
-    glm::vec3 viewVector = glm::vec3(0.f, 0.f, -1.f);
-    glm::vec3 moveVector = glm::vec3(0.f, 0.f, -1.f);
+    glm::vec3 eyePos = glm::vec3(0.f, 10.5f, -40.6f).zyx();
+    //glm::vec3 eyePos = glm::vec3(0.f, 1.0f, 10.6f);
+    glm::vec3 viewVector = glm::vec3(0.f, 0.f, 1.f).zyx();
+    glm::vec3 moveVector = glm::vec3(0.f, 0.f, 1.f).zyx();
 
     // initial matrices
     auto scale = 10.0f * glm::vec3(1.f, 1.f, 1.f);
