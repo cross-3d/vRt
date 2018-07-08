b88a0a2fde9f060e5f7543abea0d147b166d2dc2
Elv. H <elviras9t@gmail.com>
2018-07-08 16:54:07 +0800

    breaking bvh traverse early

--- shaders-next/hlBVH2/submodules/traverse-bvh2.glsl
+++ shaders-next/hlBVH2/submodules/traverse-bvh2.glsl
@@ -141,8 +141,10 @@ void traverseBvh2(in bool_ valid, in int eht) {
     ivec4 cnode = traverseState.idx >= 0 ? (texelFetch(bvhMeta, traverseState.idx)-1) : (-1).xxxx;
     for (int hi=0;hi<max_iteraction;hi++) {
         SB_BARRIER
-        IFALL (traverseState.idx < 0) break; // if traverse can't live
-        if (traverseState.idx >= 0) { for (;hi<max_iteraction;hi++) {
+        //IFALL (traverseState.idx < 0) break; // if traverse can't live
+        if (traverseState.idx < 0) break; else 
+        //if (traverseState.idx >= 0)
+        { for (;hi<max_iteraction;hi++) {
             bool _continue = false;
 
             // if not leaf and not wrong
