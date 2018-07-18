beffbb4c1ac50bf3fbe2a64286d76d081ceb5bf6
Elv. H <elviras9t@gmail.com>
2018-07-18 13:48:50 +0800

    use box diagonal size of identify 4rd axis of mortons

--- include/vRt/Utilities/VkUtils.hpp
+++ include/vRt/Utilities/VkUtils.hpp
@@ -24,7 +24,7 @@
 
 namespace _vt {
     constexpr auto DEFAULT_FENCE_TIMEOUT = 100000000000ll;
-    constexpr auto INTENSIVITY = 4096ull;
+    constexpr auto INTENSIVITY = 3600ull;
     constexpr auto ATTRIB_EXTENT = 8ull; // no way to set more than it now
 
     template <typename T>
