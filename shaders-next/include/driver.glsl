b722d7e42731a4e8b0c1061946395b269f174d62
Elv. H <elviras9t@gmail.com>
2018-09-01 02:59:14 +0800

    prefer 64 elements in basic work groups

--- shaders-next/include/driver.glsl
+++ shaders-next/include/driver.glsl
@@ -80,12 +80,7 @@
 #endif
 
 #ifndef WORK_SIZE
-    #define WORK_SIZE 128
-    //#ifdef AMD_PLATFORM
-    //    #define WORK_SIZE 64
-    //#else
-    //    #define WORK_SIZE 32
-    //#endif
+    #define WORK_SIZE 64
 #endif
 
 #define LOCAL_SIZE_LAYOUT layout(local_size_x=WORK_SIZE)in
