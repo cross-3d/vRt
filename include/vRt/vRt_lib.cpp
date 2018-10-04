624e3c287a42c568abc8c7799f03f6eb4bbbeb4f
elviras9t <elviras9t@gmail.com>
2018-10-04 16:55:52 +0000

    better implementation

--- include/vRt/vRt_lib.cpp
+++ include/vRt/vRt_lib.cpp
@@ -1,6 +1,12 @@
 #pragma once
 
-// implementator of vRt in static lib
+// unimplemented header 
+#ifndef VMA_IMPLEMENTATION
+#define VMA_IMPLEMENTATION // require to implement VMA 
+#endif
+#include "vRt.h"
+
+// dedicated implementators 
 #ifndef VRT_IMPLEMENTATION
 #define VRT_IMPLEMENTATION
 #include "Backland/vRt_impl.inl"
