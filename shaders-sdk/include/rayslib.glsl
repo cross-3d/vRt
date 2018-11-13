9d9c2ee2198adc09dc603cadc0e804189935fe60
elviras9t <elviras9t@gmail.com>
2018-11-13 19:53:33 +0800

    optimize by ray color

--- shaders-sdk/include/rayslib.glsl
+++ shaders-sdk/include/rayslib.glsl
@@ -87,7 +87,7 @@ int atomicIncAttribCount() {return atomicIncVtCounters(7);}
 
 #define rHIT hits[hitID]
 int vtReuseRays(in VtRay ray, in highp uvec2 c2d, in uint type, in lowp int rayID) {
-    [[flatten]] if (max3_vec(f16_f32(ray.dcolor)) >= 1e-4f) {
+    [[flatten]] if (max3_vec(f16_f32(ray.dcolor)) >= 0.004f) {
         parameteri(RAY_TYPE, ray.dcolor.y, int(type));
         const int rID = atomicIncRayTypedCount(rGroupDefault);//atomicIncRayCount();
         rayID = rayID < 0 ? rID : rayID; rays[rayID] = ray;
