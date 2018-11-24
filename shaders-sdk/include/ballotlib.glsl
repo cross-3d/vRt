9caa1564a70a14e926fa5947db3f54f3fde3755c
elviras9t <elviras9t@gmail.com>
2018-11-24 12:09:17 +0800

    add memory barriers

--- shaders-sdk/include/ballotlib.glsl
+++ shaders-sdk/include/ballotlib.glsl
@@ -60,9 +60,9 @@
 #define readLane RL_
 
 // subgroup barriers
-#define SB_BARRIER  //subgroupMemoryBarrier();
+#define SB_BARRIER  subgroupMemoryBarrier();
 //#ifdef NVIDIA_PLATFORM
-#define LGROUP_BARRIER subgroupBarrier();
+#define LGROUP_BARRIER subgroupMemoryBarrier(),subgroupBarrier();
 //#else
 //#define LGROUP_BARRIER barrier();
 //#endif
